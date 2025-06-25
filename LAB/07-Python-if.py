bda0dc061c34b88ca1f99dd981edf6b8c2c0bc93
TIGGER-PYTHON <tipouyu.hardline@gmail.com>
2025-06-25 18:35:24 +0700

    if else
    learn how to use if else and how it work

---
+++ LAB/07-Python-if.py
@@ -0,0 +1,5 @@
+letter = input("")
+if letter == "A":
+    print("Is A")
+else:
+    print("not A")
---
+++ LAB/08-Python-if.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("เลขคู่")
+else:
+    print("เลขคี่")
